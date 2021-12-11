4516521af7f852e697ed88022fa54891bffa4711
Jessica Nwaogbe <jessicanwaogbe96@gmail.com>
2021-12-10 22:02:30 -0500

    Modified to resolve PMD warnings

--- AnkiDroid/src/main/java/com/ichi2/anki/PreferencesSearchRecyclerAdapter.java
+++ AnkiDroid/src/main/java/com/ichi2/anki/PreferencesSearchRecyclerAdapter.java
@@ -9,43 +9,62 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+/**
+ * Preferences Search Recylcer Adapter
+ */
+// CS427 https://github.com/ankidroid/Anki-Android/issues/8342
 public class PreferencesSearchRecyclerAdapter extends RecyclerView.Adapter<PreferencesSearchRecyclerAdapter.ViewHolder> implements Filterable
 {
-    private List<PreferencesSearchOptions> preferencesList;
-    private List<PreferencesSearchOptions> preferencesListAll;
-    private PreferencesSearchListener listener;
+    /**
+     * List of preferences search options to be filtered
+     */
+    private static final List<PreferencesSearchOptions> PREF_LIST;
+    /**
+     * List of all preferences search options
+     */
+    private static final List<PreferencesSearchOptions> PREF_LIST_ALL;
+    /**
+     * Listener for search suggestion typing
+     */
+    private static final PreferencesSearchListener LISTENER;
 
-    public PreferencesSearchRecyclerAdapter( List<PreferencesSearchOptions> preferencesList, PreferencesSearchListener listener) {
-        this.preferencesList = preferencesList;
-        this.preferencesListAll = new ArrayList<>(preferencesList);
-        this.listener = listener;
+
+    /**
+     * Default constructor
+     * @param preferencesList
+     * @param LISTENER
+     */
+    public PreferencesSearchRecyclerAdapter( final List<PreferencesSearchOptions> preferencesList, final PreferencesSearchListener listener) {
+        super();
+        this.PREF_LIST = preferencesList;
+        this.PREF_LIST_ALL = new ArrayList<>(preferencesList);
+        this.LISTENER = listener;
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.preferences_search_list_item, parent, false);
+    public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+        final LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        final View view = layoutInflater.inflate(R.layout.preferences_search_list_item, parent, false);
 
-        ViewHolder viewHolder = new ViewHolder(view, listener);
-
-        return viewHolder;
+        return new ViewHolder(view, LISTENER);
     }
 
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.preferencesName.setText(preferencesList.get(position).getSearchString());
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        holder.preferencesName.setText(PREF_LIST.get(position).getSearchString());
     }
 
 
     @Override
     public int getItemCount() {
-        return preferencesList.size();
+        return PREF_LIST.size();
     }
 
 
@@ -54,23 +73,27 @@ public class PreferencesSearchRecyclerAdapter extends RecyclerView.Adapter<Prefe
         return filter;
     }
 
-    private Filter filter = new Filter() {
+
+    /**
+     * Preferences Search Options filter
+     */
+    private final Filter filter = new Filter() {
         @Override
-        protected FilterResults performFiltering(CharSequence charSequence) {
-            List<PreferencesSearchOptions> filteredList = new ArrayList<>();
+        protected FilterResults performFiltering(final CharSequence charSequence) {
+            final List<PreferencesSearchOptions> filteredList = new ArrayList<>();
 
             if (charSequence.toString().isEmpty()) {
                 filteredList.clear();
             } else {
-                for (PreferencesSearchOptions options: preferencesListAll) {
-                    String preference = options.getSearchString();
-                    if (preference.toLowerCase().contains(charSequence.toString().toLowerCase())) {
+                for (final PreferencesSearchOptions options: PREF_LIST_ALL) {
+                    final String preference = options.getSearchString();
+                    if (preference.toLowerCase(Locale.ENGLISH).contains(charSequence.toString().toLowerCase(Locale.ENGLISH))) {
                         filteredList.add(options);
                     }
                 }
             }
 
-            FilterResults filterResults = new FilterResults();
+            final FilterResults filterResults = new FilterResults();
             filterResults.values = filteredList;
 
             return filterResults;
@@ -78,35 +101,62 @@ public class PreferencesSearchRecyclerAdapter extends RecyclerView.Adapter<Prefe
 
 
         @Override
-        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
-            preferencesList.clear();
-            preferencesList.addAll((List) filterResults.values);
+        protected void publishResults(final CharSequence charSequence, final FilterResults filterResults) {
+            PREF_LIST.clear();
+            PREF_LIST.addAll((List) filterResults.values);
             notifyDataSetChanged();
         }
     };
 
-    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        private PreferencesSearchListener listener;
-        private TextView preferencesName;
 
-        public ViewHolder(@NonNull View itemView, PreferencesSearchListener listener) {
+
+    /**
+     * ViewHolder for Preferences Search Recycler Adapter
+     */
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        /**
+         * Preferences Search Listener
+         */
+        private static final PreferencesSearchListener LISTENER;
+        /**
+         * Preferences Name
+         */
+        private static final TextView PREF_NAME;
+
+
+        /**
+         * Constructor for ViewHolder
+         * @param itemView
+         * @param listener
+         */
+        public ViewHolder(@NonNull final View itemView, final PreferencesSearchListener listener) {
             super(itemView);
 
-            preferencesName = itemView.findViewById(R.id.prefs_search_name);
-            preferencesName.setElegantTextHeight(true);
+            PREF_NAME = itemView.findViewById(R.id.prefs_search_name);
+            PREF_NAME.setElegantTextHeight(true);
 
             itemView.setOnClickListener(this);
-            this.listener = listener;
+            this.LISTENER = listener;
 
         }
 
         @Override
-        public void onClick(View v) {
+        public void onClick(final View view) {
             this.listener.onSearchSuggestionSelected(getBindingAdapterPosition());
         }
     }
 
+
+
+    /**
+     * Preferences Search Listener Interface
+     */
+    // CS427 https://github.com/ankidroid/Anki-Android/issues/8342
     public interface PreferencesSearchListener {
+        /**
+         * Performs action depending on search suggestion selected
+         * @param position Index of position in search suggestion list
+         */
         void onSearchSuggestionSelected(int position);
     }
 }
